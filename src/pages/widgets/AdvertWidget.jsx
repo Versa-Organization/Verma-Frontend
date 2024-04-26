2dba524c1ce4820773b8da54dbbd8b658f3e50c7
Aman Verma <aman.verma@thegatewaycorp.co.in>
2024-04-26 12:37:26 +0530

    updated code ...

--- src/pages/widgets/AdvertWidget.jsx
+++ src/pages/widgets/AdvertWidget.jsx
@@ -1,4 +1,4 @@
-import { Typography, useTheme } from "@mui/material";
+import { Link, Typography, useTheme } from "@mui/material";
 import FlexBetween from "../../components/FlexBetween";
 import WidgetWrapper from "../../components/WidgetWrapper";
 import logo1 from "../../assets/logo1.png";
@@ -15,7 +15,7 @@ const AdvertWidget = () => {
         <Typography color={dark} variant="h5" fontWeight="500">
           Sponsored
         </Typography>
-        <Typography color={medium}>Verma</Typography>
+        <Typography color={medium}>Verma Social</Typography>
       </FlexBetween>
       <img
         width="100%"
@@ -25,8 +25,8 @@ const AdvertWidget = () => {
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
-        <Typography color={main}>Verma</Typography>
-        <Typography color={medium}>verma.com</Typography>
+        <Typography color={main}>Verma Social</Typography>
+        <Link href="https://vermasocial.vercel.app/home" target="_blank"><Typography color={medium} style={{ cursor: 'pointer' }}>vermasocial.vercel.app</Typography></Link>
       </FlexBetween>
       <Typography color={medium} m="0.5rem 0">
         "Verma" is a social media app where users can share posts, including images and thoughts, and chat with friends.
