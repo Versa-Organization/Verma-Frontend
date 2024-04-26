1ac4a642581cd6bb3eef7bfd82e931a969b6ac60
Aman Verma <aman.verma@thegatewaycorp.co.in>
2024-04-26 10:54:19 +0530

    updated Code...

--- src/pages/widgets/PostsWidget.jsx
+++ src/pages/widgets/PostsWidget.jsx
@@ -4,10 +4,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { setPosts } from "../../state/index";
 import PostWidget from "./PostWidget";
 import Scrollbars from "react-custom-scrollbars-2";
+import { useMediaQuery } from "@mui/material";
 
 const PostsWidget = ({ userId, isProfile = false, isEffect }) => {
   const scrollbarsRef = useRef(null);
   const dispatch = useDispatch();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
   const posts = useSelector((state) => state.posts);
   const token = useSelector((state) => state.token);
 
@@ -42,7 +44,7 @@ const PostsWidget = ({ userId, isProfile = false, isEffect }) => {
 
   return (
     <>
-      <Scrollbars
+      {isNonMobileScreens ? <Scrollbars
         autoHide
         autoHideTimeout={1000}
         autoHideDuration={200}
@@ -81,7 +83,38 @@ const PostsWidget = ({ userId, isProfile = false, isEffect }) => {
         ) : (
           <p>No posts found</p>
         )}
-      </Scrollbars>
+      </Scrollbars> : <>{Array.isArray(posts) && posts.length > 0 ? (
+        posts.slice().reverse().map(
+          ({
+            _id,
+            userId,
+            firstName,
+            lastName,
+            description,
+            location,
+            picturePath,
+            userPicturePath,
+            likes,
+            comments,
+          }) => (
+            <PostWidget
+              key={_id}
+              postId={_id}
+              postUserId={userId}
+              name={`${firstName} ${lastName}`}
+              description={description}
+              location={location}
+              picturePath={picturePath}
+              userPicturePath={userPicturePath}
+              likes={likes}
+              comments={comments}
+              isEffect={isEffect}
+            />
+          )
+        )
+      ) : (
+        <p>No posts found</p>
+      )}</>}
 
     </>
   );
