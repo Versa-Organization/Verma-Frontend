a15e9ebae459700e8f6fec12136488ed51d6343f
Aman Verma <aman.verma@thegatewaycorp.co.in>
2024-04-26 10:11:54 +0530

    updated code ....

--- src/utils/Socket.js
+++ src/utils/Socket.js
@@ -4,7 +4,9 @@ import { io } from "socket.io-client";
 export let socket;
 
 export function initializeSocket() {
-    socket = new io(process.env.REACT_APP_SOCKET_URL);
+    socket = io(process.env.REACT_APP_SOCKET_URL, {
+        transports: ["websocket"]
+    });
 
     socket.on("connect", () => {
         console.log("Connected to server");
